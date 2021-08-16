7cc9a631a3a94bcbd49f4be9d3bdb7b995997178
Kayla Phelps <kmphelps822@gmail.com>
2021-08-16 11:31:17 -0700

    commit tree 02d65de9fb819fad60085264cf3f75868a11e04f

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,22 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
+  const [items, setItems] = useState(itemData); // this data will be passed down to the ShoppingList as a prop
+  const [toggle, setToggle] = useState(false); // creates useState for the toggle button
+  
+  function handleToggle() { // function that handles the toggle button when it is clicked
+    setToggle((toggle) => !toggle); // takes current state of toggle and changes to the oppsite value
+  };
 
-  // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
-
+  const appClass = toggle ? "App dark" : "App light";
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleToggle}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [item, setItem] = useState(false);
+
+  function addToCart() {
+    setItem((item) => !item);
+  };
+
+  const appClass = item ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>Add to Cart</button>
     </li>
   );
 }
